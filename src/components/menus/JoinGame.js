2d9fd9635c4a2697e231a207ab0feb5ddce98787
LDK <lemondropkid@gmail.com>
2020-09-21 20:31:09 -0700

    Restyles JoinGame menu similarly to NewGame menu and allows closing the menu

--- src/components/menus/JoinGame.js
+++ src/components/menus/JoinGame.js
@@ -8,15 +8,20 @@ class JoinGameMenu extends React.Component {
 		this.state = {
 			formOpen: false,
 			gameId: null,
-			gameFound: false,
-			parentMenu: props.app.newGameMenu
+			gameFound: false
 		};
+		this.parentMenu = props.app.newGameMenu;
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.updateChosen = this.updateChosen.bind(this);
+		this.closeForm = this.closeForm.bind(this);
 		props.app.JoinGameMenu = this;
 	}
 	componentDidMount() {
-		this.state.parentMenu.getOpenGames();
+		this.parentMenu.getOpenGames();
+	}
+	closeForm() {
+		this.setState({ formOpen: false });
+		this.parentMenu.setState({ formOpen: true });
 	}
 	handleSubmit(event) {
 		event.preventDefault();
@@ -67,30 +72,35 @@ class JoinGameMenu extends React.Component {
 		}
 		var JoinGameForm = (
 			<form action={app.gameServer+"new_game"} onSubmit={this.handleSubmit}>
-				<h3 className="mb-2">Select a game to join!</h3>
-				<div>
+				<h3 className="mb-2 text-white">Select a game to join!</h3>
+				<div className="bg-white p-3 rounded mt-3">
 					<DataBrowser 
 						label="Open Games:" 
+						labelClass="d-block fw-500"
 						items={app.openGames} 
 						emptyOption='- Select A Game -'
 						emptyVal={null}
 						view="select" 
 						id="openGameList" 
 						parentObj={this}
+						className="mb-3"
 						// refName='pastOpponents'
 						callback={this.updateChosen}
 					/>
+					<input type="submit" value="Submit" size="3" onClick={this.handleSubmit} disabled={!this.state.gameFound} className="d-block pl-3 mb-3" />
 				</div>
-				<input type="submit" value="Submit" size="3" onClick={this.handleSubmit} disabled={!this.state.gameFound} />
 			</form>
 		);
 		return (
 		<Modal 
 			id="JoinGame-modal"
 			app={app}
+			closeButton={true}
 			content={JoinGameForm}
+			closeCallback={this.closeForm}
 			open={true}
-			height="medium"
+			height="auto"
+			styles={{ backgroundColor: 'rgb(56, 128, 190)' }}
 			width="small"
 			additionalClasses={"p-5 text-black"}
 		/>
